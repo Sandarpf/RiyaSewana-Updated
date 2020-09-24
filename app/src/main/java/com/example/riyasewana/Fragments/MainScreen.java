652d77542a1fa90ee862817373d6adca626512b2
shivanthi-fernando <58467116+shivanthi-fernando@users.noreply.github.com>
2020-09-25 01:27:08 +0530

    Bottom Navigation Bar

---
+++ app/src/main/java/com/example/riyasewana/Fragments/MainScreen.java
@@ -0,0 +1,63 @@
+package com.example.riyasewana.Fragments;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.riyasewana.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainScreen extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+
+        bottomNavigationView = findViewById(R.id.bottomNav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavMethod);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container_fragment, new Home()).commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener bottomNavMethod = new
+            BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                    Fragment fragment = null;
+
+                    switch (item.getItemId())
+                    {
+                        case R.id.bottom_home:
+                            fragment = new Home();
+                            break;
+
+                        case R.id.bottom_search:
+                            fragment = new Search();
+                            break;
+
+                        case R.id.bottom_favourites:
+                            fragment = new Favourites();
+                            break;
+
+                        case R.id.bottom_add:
+                            fragment = new Add();
+                            break;
+
+                        case R.id.bottom_profile:
+                            fragment = new Profile();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container_fragment,fragment).commit();
+
+                    return true;
+                }
+            };
+
+}
