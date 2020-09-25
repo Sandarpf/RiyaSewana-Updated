d7855be80e52465755622400065481ad38d5b386
shivanthi-fernando <58467116+shivanthi-fernando@users.noreply.github.com>
2020-09-25 18:43:53 +0530

    Search and add fragments

---
+++ app/src/main/java/com/example/riyasewana/Fragments/SearchParts.java
@@ -0,0 +1,28 @@
+package com.example.riyasewana.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.riyasewana.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SearchParts extends Fragment {
+
+    public SearchParts() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search_parts, container, false);
+    }
+}
