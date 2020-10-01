e2c5e3ab757c86eaf126860d599ada2f0d7e2fdd
shivanthi-fernando <58467116+shivanthi-fernando@users.noreply.github.com>
2020-10-01 09:16:04 +0530

    Add a new vehicle

--- app/src/main/java/com/example/riyasewana/Fragments/VehicleSearchAdapter.java
+++ app/src/main/java/com/example/riyasewana/Fragments/VehicleSearchAdapter.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -14,19 +16,20 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.riyasewana.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class VehicleSearchAdapter extends RecyclerView.Adapter<VehicleSearchAdapter.ViewHolder> {
+public class VehicleSearchAdapter extends RecyclerView.Adapter<VehicleSearchAdapter.ViewHolder> implements Filterable {
 
     List<VehicleModel> vehicleList;
+    List<VehicleModel> searchVehicleList;
 
 
 
+    public VehicleSearchAdapter(List<VehicleModel> vehicleList) {
 
-    public VehicleSearchAdapter(List<VehicleModel> vehiclesList) {
-
-        this.vehicleList = vehiclesList;
-
+        this.vehicleList = vehicleList;
+        searchVehicleList = new ArrayList<>(vehicleList);
     }
 
     @NonNull
@@ -59,9 +62,46 @@ public class VehicleSearchAdapter extends RecyclerView.Adapter<VehicleSearchAdap
 
     @Override
     public int getItemCount() {
-
         return vehicleList.size();
     }
+
+    @Override
+    public Filter getFilter() {
+        return vehicleSearchFilter;
+    }
+
+    private Filter vehicleSearchFilter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence charSequence) {
+
+            List<VehicleModel> filterVehicleList = new ArrayList<>();
+
+            if (charSequence == null || charSequence.length() == 0) {
+                filterVehicleList.addAll(searchVehicleList);
+            }
+            else {
+                String filterPattern = charSequence.toString().toLowerCase().trim();
+
+                for (VehicleModel vehicle : searchVehicleList) {
+                    if (vehicle.getVehicle_name().toLowerCase().contains(filterPattern)) {
+                        filterVehicleList.add(vehicle);
+                    }
+                }
+            }
+
+            FilterResults results = new FilterResults();
+            results.values = filterVehicleList;
+
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+            vehicleList.clear();
+            vehicleList.addAll((List) filterResults.values);
+            notifyDataSetChanged();
+        }
+    };
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
